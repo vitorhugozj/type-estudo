31b50e4451b1dcb41fe5e8bad1fa0529ba01ee48
VITOR HUGO NUNES NASCIMENTO <23111767@CFP127ALUNO.LOCAL>
2024-08-22 11:40:43 -0300

    rodar as dependencias

---
+++ aula01/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>
+      Olá 
+    </h1>
+  </React.StrictMode>
+)
